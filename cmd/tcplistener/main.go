ebe3c4ab1090f99c741c120b9906f17f35aad551
Amir Aminbeidokhti <amir.aminbeidokhti@koho.ca>
2026-03-29 18:07:04 -0600

    CH4: L6 - Connect the Parsing

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,49 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/amiraminb/HTTPfromTCP/internal/request"
 )
-
-func getLinesChannel(conn net.Conn) <-chan string {
-	ch := make(chan string, 1)
-
-	go func() {
-		defer close(ch)
-		defer conn.Close()
-		buf := make([]byte, 8)
-		current_line := ""
-		for {
-			n, err := conn.Read(buf)
-			data := buf[:n]
-
-			for {
-				idx := bytes.IndexByte(data, '\n')
-
-				if idx == -1 {
-					current_line += string(data)
-					break
-				}
-
-				current_line += string(data[:idx])
-				ch <- current_line
-				current_line = ""
-				data = data[idx+1:]
-			}
-
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}()
-	return ch
-}
 
 func main() {
 	listener, err := net.Listen("tcp", "localhost:42069")
@@ -58,13 +21,14 @@ func main() {
 			log.Fatal(err)
 		}
 
-		fmt.Println("A connection has been accepted...")
-
-		ch := getLinesChannel(conn)
-		for data := range ch {
-			fmt.Println(data)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		fmt.Println("The connection is closed...")
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target :%s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 	}
 }
